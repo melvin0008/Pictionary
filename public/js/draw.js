193baf58b83172ef72fcaca2186c591290a80ce9
Melvin <melvin0008@gmail.com>
2015-02-14 18:03:17 +0530

    added comments

--- public/js/draw.js
+++ public/js/draw.js
@@ -1,22 +1,30 @@
 var pathArray;
 var myPath;
+
+
+//Start creating path
 function onMouseDown(event) {
+
   myPath= new Path();
   myPath.strokeColor = 'black';
   pathArray=[event.point];
+  drawLine(event.point);
 }
 
+//Continue Drawing points
 function onMouseDrag(event) {
   drawLine(event.point);
   pathArray.push(event.point);
 }
 
-function onMouseUp(event) {
+//Finish Drawing point . Send it to others.
+function onMouseUp(event) {  
   pathArray.push(event.point);
   drawLine(event.point);
   emitLine(pathArray);
   pathArray= new Array(); 
 }
+// Draw points as they come in.
 function drawLine(point) {
   myPath.add(point);
   myPath.smooth();
@@ -45,5 +53,4 @@ function drawOnClientEnd(data)
     myPath.close=true;
     myPath.smooth();
     view.draw();
-    console.log("Drawn");
 }
