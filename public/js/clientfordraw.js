2ef287d2da808b802b70eab97431df5644648b97
Melvin <melvin0008@gmail.com>
2015-02-20 01:43:55 +0530

    chat working

--- public/js/clientfordraw.js
+++ public/js/clientfordraw.js
@@ -1,113 +1,83 @@
 var socket = io.connect("http://localhost:3000");
 // var socket = io.connect("https://piction.herokuapp.com");
+var FADE_TIME = 150; 
+var username="temp"+(Math.floor((Math.random() * 100) + 1)).toString();
+var room =window.location.href;
+var res=room.split("/");
+var roomname=res[4];
+var sessionId = socket.io.engineid;
+
 
 $(document).ready(function() {
 	$('#chatexpand').click(function() {
 		$('#chatwindow').slideToggle("fast");
+    // $("i",this).toggleClass("fa fa-plus fa fa-minus");
 	});
 
 
 var $window = $(window);
-var $messages = $('.messages');
+var $messages = $('.discussion');
 var $inputMessage = $('#inputMessage');
 
-function sendMessage () {
+var sendMessage =function(opt,username,message) {
 
-    var message = $('#inputMessage').val();
-    // Prevent markup from being injected into the message
-//    message = cleanInput(message);
-console.log(message);
-    // if there is a non-empty message and a socket connection
     if (message) {
-      console.log(message);
       $inputMessage.val('');
       addChatMessage({
         username: username,
         message: message
-      });
-      // tell server to execute 'new message' and send along one parameter
-//      socket.emit('new message', message);
+      },opt);
+    
     }
 }
-function cleanInput (input) {
-    return $('<div/>').text(input).text();
-}
+
 
   // Adds the visual chat message to the message list
-function addChatMessage (data, options) {
+var addChatMessage =function(data,opt) {
     // Don't fade the message in if there is an 'X was typing'
-    options = options || {};
-
-    var $usernameDiv = $('<span class="username"/>')
-      .text(data.username)
-      .css('color', getUsernameColor(data.username));
-    var $messageBodyDiv = $('<span class="messageBody">')
+    var $usernameDiv = $('<label class="avatar"/>')
+      .text(data.username);
+    var $messageBodyDiv = $('<div class="messages">')
       .text(data.message);
-
-    var $messageDiv = $('<li class="message"/>')
-      .data('username', data.username)
-      .addClass(typingClass)
+    var $messageDiv = $('<li class='+opt+'/>')
       .append($usernameDiv, $messageBodyDiv);
-
-    addMessageElement($messageDiv, options);
+      addMessageElement($messageDiv);
 }
 
   $window.keydown(function (event) {
-    // Auto-focus the current input when a key is typed
-  
-    // When the client hits ENTER on their keyboard
     if (event.which === 13) {
-      // if (true) {
-        sendMessage();
 
-      // }
+        var message = $('#inputMessage').val();
+        sendMessage("self",username,message);
+        socket.emit('new_message',sessionId, {message:message,user:username,room:roomname});
     }
   });
 
-function addMessageElement (el, options) {
+function addMessageElement (el) {
     var $el = $(el);
-
-    // Setup default options
-    if (!options) {
-      options = {};
-    }
-    if (typeof options.fade === 'undefined') {
-      options.fade = true;
-    }
-    if (typeof options.prepend === 'undefined') {
-      options.prepend = false;
-    }
-
-    // Apply options
-    if (options.fade) {
-      $el.hide().fadeIn(FADE_TIME);
-    }
-    if (options.prepend) {
-      $messages.prepend($el);
-    } else {
-      $messages.append($el);
-    }
+    $messages.append($el);
     $messages[0].scrollTop = $messages[0].scrollHeight;
   }
+  socket.on('connect', function(){
+      
+      socket.emit("joinroom",sessionId,{room:roomname,user:username});
+      console.log("Connected");
+
+      socket.on('displayerror',function(error)
+      {
+        $('#errorstring').html(error);
+        $('#error').modal('show');
+      });
+      
+
+      socket.on('othermessage',function(data)
+      {
+        sendMessage("other",data.user,data.message);
+      });
+});
+
 
 });
 
 
 
-socket.on('connect', function(){
-			// firing back the connect event to the server
-			// and sending the nickname for the connected client
-			var room =window.location.href;
-			var res=room.split("/");
-			var roomname=res[4];
-			var username="temp"+(Math.floor((Math.random() * 100) + 1)).toString();
-			var sessionId = socket.io.engineid;
-			socket.emit("joinroom",sessionId,{room:roomname,user:username});
-			console.log("Connected");
-
-			socket.on('displayerror',function(error)
-			{
-				$('#errorstring').html(error);
-				$('#error').modal('show');
-			});
-});
